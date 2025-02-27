d8944b075f66374b9cb7afb8eac611eefa8efd34
Dober09 <lebohanglehutjo@gmail.com>
2025-02-27 16:48:00 +0200

    Add project files.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,81 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController() { }
+
+        // get all action
+        [HttpGet(Name = "pizza")]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        // get by id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+
+
+        // post action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            // this code will save the pizza abd return result
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
+        //put action
+        [HttpPut("{id}")]
+        public IActionResult Updata(int id,Pizza pizza) {
+            
+            if(id == pizza.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingPizza = PizzaService.Get(id);
+
+            if(existingPizza is null)
+            {
+                return NotFound();
+            }
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        //delete action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //3
+            var pizza = PizzaService.Get(id);
+
+            if(pizza is null)
+            {
+                return NotFound();
+            }
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ Models/Pie.cs
@@ -0,0 +1,12 @@
+﻿namespace ContosoPizza.Models
+{
+    public class Pie
+    {
+        public int Id { get; set; }
+        public  string? Name { get; set; }
+        public  string? Origin { get; set; }
+        public  string? TypeOfPie { get; set; }
+        public  bool IsAvailable { get; set; }
+
+    }
+}
